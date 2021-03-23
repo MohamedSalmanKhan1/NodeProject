e74b24ccf0a6cee012c4eefdc2de501ca86e6aff
MohamedSalmanKhan1 <78850437+MohamedSalmanKhan1@users.noreply.github.com>
2021-03-23 19:36:53 +0530

    Delete Http.js

--- Http.js
+++
@@ -1,6 +0,0 @@
-var http=require('http');
-http.createServer(function(req,res){
-    res.writeHead(200,{'Content-type':'text/plain'});
-    res.write("Salman Software Engineer");
-    res.end("Thank You");
-}).listen(8080);
