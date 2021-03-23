599c44ea2eef6604c13d68aec08993027d5b9cb3
MohamedSalmanKhan1 <78850437+MohamedSalmanKhan1@users.noreply.github.com>
2021-03-23 19:38:16 +0530

    Delete NodeMySQL directory

--- NodeMySQL/server.js
+++
@@ -1,8 +0,0 @@
-const mysql=require("mysql");
-const express=require("express");
-const ebodyParser=require("body-parser");
-const bodyParser = require("body-parser");
-
-var app=express();
-
-app.use(bodyParser.json());
